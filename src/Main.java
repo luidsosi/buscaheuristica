4b1a0cc3308e6840264deb7ecd96ec8766034501
luidsosi <luidsosi@gmail.com>
2019-08-18 15:44:22 -0300

    calcula o tempo de execucao

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,10 @@ public class Main {
 		System.out.println(oitoRainhas.getQuantidadeAtaques());
 		oitoRainhas.printPosicaoRainhas();
 		
+		long tempoInicial = System.currentTimeMillis();
 		oitoRainhas.subidaEncosta2();
+		long tempoFinal = System.currentTimeMillis();
+		long tempoExecucao = tempoFinal - tempoInicial;
+		System.out.println("Tempo de execução: " + tempoExecucao + "ms");
 	}
 }
